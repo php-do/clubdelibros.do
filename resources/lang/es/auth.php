93dc9b886c2512c04944684d5477cba46855f918
christianrd <me@christiandr.com>
2017-02-14 00:04:06 -0400

    Languague spanish added by default and timezone

---
+++ resources/lang/es/auth.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: www.christiandr.com
+ * Date: 2/13/2017
+ * Time: 11:15 PM
+ */
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+    'failed'   => 'Estas credenciales no coinciden con nuestros registros.',
+    'throttle' => 'Demasiados intentos de acceso. Por favor intente nuevamente en :seconds segundos.',
+];
